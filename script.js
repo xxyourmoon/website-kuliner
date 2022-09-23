057fc08da83f2532ff25867c42a68e8918f1e312
Jyriz <discordketukangan123@gmail.com>
2022-09-23 14:33:40 +0700

    Update 23/09/2022

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-fetch("./data/index.json")
-   .then((response) => response.json())
-   .then((json) => console.log(json));
+// fetch("./data/index.json")
+//    .then((response) => response.json())
+//    .then((json) => console.log(json));
+console.log(window.innerHeight);
+console.log(window.innerWidth);
