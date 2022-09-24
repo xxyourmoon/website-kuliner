b0e276a23a479e1d0eff9037e734db155ae8ed8a
Jyriz <discordketukangan123@gmail.com>
2022-09-24 08:03:31 +0700

    Update JSON Data & Give the example json take data

---
+++ example-takeData.js
@@ -0,0 +1,10 @@
+fetch("./data/index.json")
+    .then((response) => response.json())
+    .then((json) => {
+        let namaToko = json.kuliner.map((e) => {    // ngelepas object dari array kuliner
+            return e.toko       // mengembalikan nilai object yang sudah di keluarkan dengan property toko
+        })
+        console.log(namaToko)
+    });
+
+    // UNTUK OUTPUT BISA DI LIHAT DI CONSOLE GUGEL
