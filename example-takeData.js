dbe93ea89c1f3e1274349d568812b7cb131e68d5
Jyriz <discordketukangan123@gmail.com>
2022-09-27 11:25:40 +0700

    UPDATE : 27/09/22 : + Resign Main Page + Added auto grid : makanan

--- example-takeData.js
+++ example-takeData.js
@@ -1,11 +1,54 @@
-// fetch("./data/index.json")
-//   .then((response) => response.json())
-//   .then((json) => {
-//     let namaToko = json.kuliner.map((e) => {
-//       // ngelepas object dari array kuliner
-//       return e.toko; // mengembalikan nilai object yang sudah di keluarkan dengan property toko
+// function getImage() {
+//   return fetch("./data/index.json")
+//     .then((response) => response.json())
+//     .then((json) => {
+//         let gambarMakanan = json.kuliner.map((e) => {
+//             if (e.gambar[0] === undefined) {
+//                 return 'None'
+//             } else {
+//                 return e.gambar[0]
+//             }
+//         })
+//         return gambarMakanan
 //     });
-//     console.log(namaToko);
+// }
+
+// function runProgram() {
+//     getImage()
+//         .then((e) => {
+            
+//         })
+// }
+
+// runProgram()
+
+//   let namaToko = json.kuliner.map((e) => {
+//     // ngelepas object dari array kuliner
+//     return e.toko; // mengembalikan nilai object yang sudah di keluarkan dengan property toko
 //   });
+//   console.log(namaToko);
 
 // UNTUK OUTPUT BISA DI LIHAT DI CONSOLE GUGEL
+
+
+$.ajax({
+    url: "./data/index.json",
+    success: results => {
+        const dataKuliner = results.kuliner;
+        let cards = ''
+        dataKuliner.forEach((e) => {
+            cards += `<li>
+                        <figure>
+                            <img src="${e.gambar[0]}"/>
+                            <figcaption>
+                                <h3>${e.toko}</h3>
+                            </figcaption>
+                        </figure>
+                    </li>`;
+        });
+        $('ul.grid-makananMantab').html(cards)
+    },
+    error: (e) => {
+        console.log(e)
+    }
+})
--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+// // fetch("./data/index.json")
+// //   .then((response) => response.json())
+// //   .then((json) => {
+// //     let namaToko = json.kuliner.map((e) => {
+// //       // ngelepas object dari array kuliner
+// //       return e.toko; // mengembalikan nilai object yang sudah di keluarkan dengan property toko
+// //     });
+// //     // namaToko.forEach(element => {
+      
+// //     //   console.log(element);
+// //     // });
+// //   });
 // fetch("./data/index.json")
 //   .then((response) => response.json())
 //   .then((json) => {
-//     let namaToko = json.kuliner.map((e) => {
-//       // ngelepas object dari array kuliner
-//       return e.toko; // mengembalikan nilai object yang sudah di keluarkan dengan property toko
-//     });
-//     // namaToko.forEach(element => {
-      
-//     //   console.log(element);
-//     // });
+//     console.table(json.kuliner)
 //   });
-fetch("./data/index.json")
-  .then((response) => response.json())
-  .then((json) => {
-    console.table(json.kuliner)
-  });
